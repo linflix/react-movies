9baf354d6c2d037849f7f880add4c8d04540ea35
linhu <1318066698@qq.com>
2020-03-23 13:32:22 +0800

    完成分页功能

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -14,23 +14,60 @@ import {
 import API from '../../configs/api';
 import VideoPlay from '../video/VideoPlay';
 const { Header, Footer, Content } = Layout;
-function MYV() {
-    let id = "test";
-    console.log(id);
-};
 export default class Home extends React.Component{
+   
     constructor(props){
         super(props);
         this.state = { 
-            movies:[]
+            total:0,
+            currentIndex:0,
+            pageSize:12,
+            videos:[]
          };
     }
     componentWillMount(){
-        API.get('movies/list').then(res=>{
+        API.get('video/list',{
+            params:{
+                currentIndex:this.state.currentIndex,
+                pageSize:this.state.pageSize
+            }
+        }).then(res=>{
             let result = res.data;
-            if(result.code === 200){
+            console.log(result);
+            if(result.success===true){
                 this.setState({
-                    movies:result.data
+                    videos:result.data
+                });
+            }
+        })
+        API.get("video/total").then(res=>{
+            let result = res.data;
+            console.log(result);
+            if(result.success===true){
+                console.log(result.data)
+                this.setState({
+                    total:result.data
+                });
+            }
+        })
+    }
+     /**
+     * 当页面发生变化的时候 需要触发。
+     */
+    pageChange(currentPage){
+        console.log("i was called ",currentPage);
+        let index = (currentPage-1)*this.state.pageSize;
+        API.get('video/list',{
+            params:{
+                currentIndex:index,
+                pageSize:this.state.pageSize
+            }
+        }).then(res=>{
+            let result = res.data;
+            console.log(result);
+            if(result.success===true){
+                this.setState({
+                    videos:result.data
                 });
             }
         })
@@ -44,17 +81,17 @@ export default class Home extends React.Component{
                     <div style={{float:"left",display:"inline"}}>
                         <div>
                             {
-                                this.state.movies.map((item,index)=>{
+                                this.state.videos.map((item,index)=>{
                                     console.log(item);
                                     return(
                                         <Link to={"/video/"+item.mId}>
-                                        <VideoCard key={index} movie={JSON.stringify(item)}/>
+                                            <VideoCard key={index} movie={JSON.stringify(item)}/>
                                         </Link>
                                     );
                                 })
                             }
                         </div>
-                        <VideoPagination/>
+                        <VideoPagination handleChangePage={this.pageChange.bind(this)} total = {this.state.total}/>
                     </div>
                 </Content>
                 <Footer>
--- src/pages/pagination/VideoPagination.js
+++ src/pages/pagination/VideoPagination.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import { Pagination } from 'antd';
+import { handleRateChange } from 'video-react/lib/actions/video';
 
-function showTotal(total) {
-    return `Total ${total} items`;
-}
 
 export default class VideoPagination extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            defaultCurrent:1,
+            defaultPageSize:12
+        }
+    }
     render(){
         return(
-            <div style={{bottom:"1%",width:"100%",float:"right",paddingLeft:"60%"}}>
-                <Pagination size="small" total={50} showSizeChanger showQuickJumper />
+            <div style={{ bottom: "1%", width: "100%", float: "right", paddingLeft: "60%" }}>
+                <Pagination defaultPageSize={this.state.defaultPageSize} showQuickJumper 
+                defaultCurrent={this.state.defaultCurrent} total={this.props.total} 
+                onChange={this.onChange.bind(this)} />
             </div>
         );
+    }
+    onChange(targetPage) {
+        console.log('Page: ', targetPage);   
+        this.setState({
+            defaultCurrent:targetPage
+        });
+        this.props.handleChangePage(targetPage);
     }
 }
